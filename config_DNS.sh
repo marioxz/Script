160e2e50ee52e22c72339bbb180448d1c4ec2ab7
mrxz <8956706+marioxz@users.noreply.github.com>
2018-03-23 19:41:32 +0700

    Update config_DNS.sh

--- config_DNS.sh
+++ config_DNS.sh
@@ -152,6 +152,7 @@ sudo sed -i '/^ftp/d' ${dir_reverse}
 sudo sed -i '/^mail/d' ${dir_reverse}
 sudo sed -i '/^@\tIN\tA/d' ${dir_reverse}
 sudo echo -e "$IP_cut\tIN\tPTR\t$domain." >> ${dir_reverse}
+sudo service bind9 restart
 
 # ask to reboot
 echo "Need reboot!!"
